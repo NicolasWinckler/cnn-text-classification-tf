ede90eeaf23320a673c01a7a604c2fe362fa976b
Nicolas Winckler <nw@quanox.com>
2017-07-21 18:10:54 +0200

    test multiclass

--- train.py
+++ train.py
@@ -9,7 +9,25 @@ import data_helpers
 from text_cnn import TextCNN
 from tensorflow.contrib import learn
 import yaml
-import math
+
+
+# helper function 
+
+
+def write_param_to_tf_board(out_directory,flagscontainer, text_list = []):
+    #text = []
+    for attr, value in sorted(flagscontainer.__flags.items()):
+        ele_ar = [str(attr.upper()),str(value)]
+        text_list.append(ele_ar)
+
+    #text = [["a", "1"], ["b", "2"], ["c", "3"]]
+    summary_op = tf.summary.text('Setting', tf.convert_to_tensor(text_list))
+    #out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", "test_tfboard"))
+    summary_writer = tf.summary.FileWriter(out_directory, sess.graph)
+    text_list = sess.run(summary_op)
+    summary_writer.add_summary(text_list, 0)
+    summary_writer.flush()
+    summary_writer.close()
 
 # Parameters
 # ==================================================
@@ -18,12 +36,12 @@ import math
 tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training data to use for validation")
 
 # Model Hyperparameters
-tf.flags.DEFINE_boolean("enable_word_embeddings", True, "Enable/disable the word embedding (default: True)")
+tf.flags.DEFINE_boolean("enable_word_embeddings", False, "Enable/disable the word embedding (default: True)")
 tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
 tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
-tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
-tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 0.0)")
+tf.flags.DEFINE_float("dropout_keep_prob", 0.3, "Dropout keep probability (default: 0.5)")
+tf.flags.DEFINE_float("l2_reg_lambda", 0.1, "L2 regularization lambda (default: 0.0)")
 
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
@@ -34,7 +52,6 @@ tf.flags.DEFINE_integer("num_checkpoints", 5, "Number of checkpoints to store (d
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
-tf.flags.DEFINE_float("decay_coefficient", 2.5, "Decay coefficient (default: 2.5)")
 
 FLAGS = tf.flags.FLAGS
 FLAGS._parse_flags()
@@ -46,11 +63,13 @@ print("")
 with open("config.yml", 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
 
-dataset_name = cfg["datasets"]["default"]
+#dataset_name = cfg["datasets"]["default"]
+dataset_name = "localdata" #cfg["datasets"]["default"]
 if FLAGS.enable_word_embeddings and cfg['word_embeddings']['default'] is not None:
     embedding_name = cfg['word_embeddings']['default']
     embedding_dimension = cfg['word_embeddings'][embedding_name]['dimension']
 else:
+    embedding_name = "internal w2v"
     embedding_dimension = FLAGS.embedding_dim
 
 # Data Preparation
@@ -78,6 +97,12 @@ x_text, y = data_helpers.load_data_labels(datasets)
 max_document_length = max([len(x.split(" ")) for x in x_text])
 vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
 x = np.array(list(vocab_processor.fit_transform(x_text)))
+class_number = len(y[0])
+
+
+
+print("Max doc length : {:d}".format(max_document_length))
+print("Number of distinct labels : {:d}".format(class_number))
 
 # Randomly shuffle data
 np.random.seed(10)
@@ -85,13 +110,20 @@ shuffle_indices = np.random.permutation(np.arange(len(y)))
 x_shuffled = x[shuffle_indices]
 y_shuffled = y[shuffle_indices]
 
+
 # Split train/test set
 # TODO: This is very crude, should use cross-validation
 dev_sample_index = -1 * int(FLAGS.dev_sample_percentage * float(len(y)))
 x_train, x_dev = x_shuffled[:dev_sample_index], x_shuffled[dev_sample_index:]
 y_train, y_dev = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
+
+vocab_size = len(vocab_processor.vocabulary_)
+y_train_size = len(y_train)
+y_dev_size = len(y_dev)
+
 print("Vocabulary Size: {:d}".format(len(vocab_processor.vocabulary_)))
 print("Train/Dev split: {:d}/{:d}".format(len(y_train), len(y_dev)))
+
 
 
 # Training
@@ -114,7 +146,7 @@ with tf.Graph().as_default():
 
         # Define Training procedure
         global_step = tf.Variable(0, name="global_step", trainable=False)
-        optimizer = tf.train.AdamOptimizer(cnn.learning_rate)
+        optimizer = tf.train.AdamOptimizer(1e-3)
         grads_and_vars = optimizer.compute_gradients(cnn.loss)
         train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
 
@@ -132,7 +164,24 @@ with tf.Graph().as_default():
         timestamp = str(int(time.time()))
         out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
         print("Writing to {}\n".format(out_dir))
-
+        
+        input_param = []
+        input_param.append(["DATA SET NAME",str(dataset_name)])
+        input_param.append(["LABELED CLASS NB",str(class_number)])
+        input_param.append(["MAX NB OF WORDS/DOC",str(max_document_length)])
+        input_param.append(["TRAINING SET SIZE",str(y_train_size)])
+        input_param.append(["TEST SET SIZE",str(y_dev_size)])
+        
+        if dataset_name == "localdata":
+            input_param.append(["CLUSTERS PATH",str(cfg["datasets"][dataset_name]["container_path"])])
+            input_param.append(["CLUSTERS NAMES",str(cfg["datasets"][dataset_name]["categories"])])
+            
+        input_param.append(["EMBEDDINGS METHOD",str(embedding_name)])
+        input_param.append(["EMBEDDINGS DIMENSION",str(embedding_dimension)])
+        
+        write_param_to_tf_board(out_dir,FLAGS,input_param)
+        
+        
         # Summaries for loss and accuracy
         loss_summary = tf.summary.scalar("loss", cnn.loss)
         acc_summary = tf.summary.scalar("accuracy", cnn.accuracy)
@@ -141,12 +190,13 @@ with tf.Graph().as_default():
         train_summary_op = tf.summary.merge([loss_summary, acc_summary, grad_summaries_merged])
         train_summary_dir = os.path.join(out_dir, "summaries", "train")
         train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
+        #inputpar_summary = tf.summary.text('input_param', tf.convert_to_tensor('hello world'))
 
         # Dev summaries
         dev_summary_op = tf.summary.merge([loss_summary, acc_summary])
         dev_summary_dir = os.path.join(out_dir, "summaries", "dev")
         dev_summary_writer = tf.summary.FileWriter(dev_summary_dir, sess.graph)
-
+        
         # Checkpoint directory. Tensorflow assumes this directory already exists so we need to create it
         checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
         checkpoint_prefix = os.path.join(checkpoint_dir, "model")
@@ -178,22 +228,20 @@ with tf.Graph().as_default():
                 print("glove file has been loaded\n")
             sess.run(cnn.W.assign(initW))
 
-        def train_step(x_batch, y_batch, learning_rate):
+        def train_step(x_batch, y_batch):
             """
             A single training step
             """
             feed_dict = {
               cnn.input_x: x_batch,
               cnn.input_y: y_batch,
-              cnn.dropout_keep_prob: FLAGS.dropout_keep_prob,
-              cnn.learning_rate: learning_rate
+              cnn.dropout_keep_prob: FLAGS.dropout_keep_prob
             }
             _, step, summaries, loss, accuracy = sess.run(
                 [train_op, global_step, train_summary_op, cnn.loss, cnn.accuracy],
                 feed_dict)
             time_str = datetime.datetime.now().isoformat()
-            print("{}: step {}, loss {:g}, acc {:g}, learning_rate {:g}"
-                  .format(time_str, step, loss, accuracy, learning_rate))
+            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
             train_summary_writer.add_summary(summaries, step)
 
         def dev_step(x_batch, y_batch, writer=None):
@@ -216,17 +264,10 @@ with tf.Graph().as_default():
         # Generate batches
         batches = data_helpers.batch_iter(
             list(zip(x_train, y_train)), FLAGS.batch_size, FLAGS.num_epochs)
-        # It uses dynamic learning rate with a high value at the beginning to speed up the training
-        max_learning_rate = 0.005
-        min_learning_rate = 0.0001
-        decay_speed = FLAGS.decay_coefficient*len(y_train)/FLAGS.batch_size
         # Training loop. For each batch...
-        counter = 0
         for batch in batches:
-            learning_rate = min_learning_rate + (max_learning_rate - min_learning_rate) * math.exp(-counter/decay_speed)
-            counter += 1
             x_batch, y_batch = zip(*batch)
-            train_step(x_batch, y_batch, learning_rate)
+            train_step(x_batch, y_batch)
             current_step = tf.train.global_step(sess, global_step)
             if current_step % FLAGS.evaluate_every == 0:
                 print("\nEvaluation:")
